4e08f96ef815aa47020cf16689fcc65e7eb163c6
Shawn Wollenberg <shawnwollenberg@users.noreply.github.com>
2020-08-18 12:50:15 -0400

    adding Random String

---
+++ randomString/randomString.go
@@ -0,0 +1,24 @@
+package randomstring
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func stringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+//RandomChars returns specified length of random chars as string
+func RandomChars(length int) string {
+
+	return stringWithCharset(length, charset)
+}
